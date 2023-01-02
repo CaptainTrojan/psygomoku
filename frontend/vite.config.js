9060749512fb8faf5576f9d56e560d013da8057d
CaptainTrojan <49991681+CaptainTrojan@users.noreply.github.com>
2023-01-02 12:06:35 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
     }
   },
   server: {
-    host: true,
+    host: 0.0.0.0,
     port: 8080,
     strictPort: true,
     headers: { 'Access-Control-Allow-Origin': '*' } //TODO !
